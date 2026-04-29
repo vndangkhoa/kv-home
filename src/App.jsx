37650b764ddc877969ef2a770468b7a1ba951866
vndangkhoa <vndangkhoa@khoavo.myds.me>
2026-04-29 15:41:41 +0700

    use desktop reference colors for mobile blinking

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,7 @@ function TetrisPiece({ piece, isDark }) {
     setShowText(false);
   };
   
-  const bgColor = piece.label === 'cv' 
-    ? piece.color 
-    : (isDark ? piece.colorDark : piece.color);
+  const bgColor = piece.label === 'cv' ? piece.color : piece.color;
   const rowDelay = piece.startY * 4;
   const textColor = '#ffffff';
   const isStatic = piece.label === 'cv' || piece.featured;
